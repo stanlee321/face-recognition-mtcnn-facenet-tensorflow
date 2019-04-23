593bf7cc5a9e361cfc4efe24d9baf2b80cd4e569
alvarohurtadobo <a.hurtado.bo@gmail.com>
2019-04-23 13:25:02 -0400

    Added argparser and logging module

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from src.face_alig import FaceAlign
-from src.tracker.sort import Sort
+# !/usr/bin/python3
 
-import numpy as np
-import time
 import cv2
+import time
+import logging
+import argparse
+import numpy as np
+
+from src.tracker.sort import Sort
+from src.face_alig import FaceAlign
+
+parser = argparse.ArgumentParser(description="Introduce the parameters")
+parser.add_argument('-i', '--input',  type = str,  default = '0',   help = 'Introduce the path to the video or source')
+parser.add_argument('-d', '--debug',  type = bool, default = False, help = 'Save debug info to log file')
+parser.add_argument('-W', '--width',  type = int,  default = 320, help = 'Introduce Width')
+parser.add_argument('-H', '--height', type = int,  default = 240, help = 'Introduce Height')
+args = parser.parse_args()
+
+my_level = args.debug
+logging.basicConfig(handlers = [  logging.FileHandler('./face_log.txt',mode='w'),
+                                logging.StreamHandler()], 
+                    format = '%(asctime)s : %(message)s',     # format='%(name)s: %(levelname)s - %(asctime)s : %(message)s'
+                    level = my_level)
 
 def main():
-    image_size = (320,240)
+    image_size = (args.width,args.height)
     margin = 40
     face_align = FaceAlign(image_size, margin)
     directoryname = 'images/'
@@ -19,8 +36,10 @@ def main():
     show_rate = 0.9  # if set it smaller will dispaly smaller frames
 
     cv2.namedWindow(kWinName, cv2.WINDOW_NORMAL)
-    src = '/home/alvaro/trafficFlow/trialVideos/video03.mp4'
-    cap = cv2.VideoCapture(src)
+    try:
+        cap = cv2.VideoCapture(args.input)
+    except:
+        logging.error('Could not load source')
     frame_interval = 3  # interval how many frames to make a detection,you need to keep a balance between performance and fluency
     c=0
     
@@ -55,7 +74,7 @@ def main():
         #frame = face_align.annodate_image_simple(frame, trackers)
 
         duration = time.time() - start_time
-        print(duration)
+        logging.debug('Last duration: {0:.2f}'.format(duration))
         #frame = cv2.resize(frame, (640, 480))
         
         frame = cv2.resize(frame, (0, 0), fx=show_rate, fy=show_rate)
